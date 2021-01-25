56f735a5d0922460bca803fdd748541778c51ad0
sakura-rip <okaoka1567@gmail.com>
2021-01-25 18:22:13 +0900

    add: DatabaseGroup struct

---
+++ server/talkDatabase/group_struct.go
@@ -0,0 +1,16 @@
+package talkDatabase
+
+type Group struct {
+	CreatorMID  string   `bson:"creator"`
+	CreatedTime int64    `bson:"createdTime"`
+	MemberMids  []string `bson:"members"`
+	InvitedMids []string `bson:"invited"`
+
+	Name        string `bson:"name"`
+	Description string `bson:"description"`
+	IconPath    string `bson:"iPath"`
+	CoverPath   string `bson:"cPath"`
+
+	GroupTicket            string `bson:"gTicket"`
+	AllowJoinByGroupTicket bool   `bson:"ajByGTicket"`
+}

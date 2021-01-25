fa2d7ef542d7b3a25704f447d18db2e1f500ac85
sakura-rip <okaoka1567@gmail.com>
2021-01-25 18:11:45 +0900

    change: modify group settings

--- server/talkDatabase/struct.go
+++ server/talkDatabase/struct.go
@@ -7,23 +7,28 @@ type User struct {
 	Profile Profile `bson:"profile"`
 	Setting Setting `bson:"setting"`
 
-	JoinedGroupIds  map[string]GroupSetting `bson:"jGroupIds"`
-	InvitedGroupIds []string                `bson:"iGroupIds"`
-	FriendIds       []string                `bson:"friendIds"`
-	BlockedIds      []string                `bson:"blockedIds"`
+	JoinedGroupIds  []string `bson:"jGroupIds"`
+	InvitedGroupIds []string `bson:"iGroupIds"`
+	FriendIds       []string `bson:"friendIds"`
+	BlockedIds      []string `bson:"blockedIds"`
 
-	Tags     map[string]Tag     `bson:"tags"`
-	Contacts map[string]Contact `bson:"contacts"`
+	Groups   map[string]GroupSetting `bson:"groups"`
+	Tags     map[string]Tag          `bson:"tags"`
+	Contacts map[string]Contact      `bson:"contacts"`
 }
+
 type GroupSetting struct {
 	//新規メッセージの通知など
-	EnableNotification bool `bson:""`
+	EnableNotification bool `bson:"enNotify"`
 	//メンションの通知
-	EnableNotificationMention bool `bson:""`
+	EnableNotificationMention bool `bson:"enNotifyMention"`
 	//誰かが新規参加した時の通知
-	EnableNotificationOnJoin bool `bson:""`
+	EnableNotificationOnJoin bool `bson:"enNotifyJoin"`
 	//誰かが誰かをキックした時の通知
-	EnableNotificationOnKick bool `bson:""`
+	EnableNotificationOnKick bool `bson:"enNotifyKick"`
+
+	OverWrittenName string   `bson:"owName"`
+	TagIds          []string `bson:"tagIds"`
 }
 type Profile struct {
 	Name      string `bson:"name"`

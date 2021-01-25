3880b4f95fa9aad42ba0228783e196c25e6501e3
sakura-rip <okaoka1567@gmail.com>
2021-01-25 21:52:52 +0900

    add: FetchGroup

--- server/talkDatabase/groups.go
+++ server/talkDatabase/groups.go
@@ -1,6 +1,38 @@
 package talkDatabase
 
+import (
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
 func (cl *DBClient) InsertNewGroup(group *Group) error {
 	_, err := cl.GroupCol.InsertOne(cl.Ctx, group)
 	return err
+}
+
+func (cl *DBClient) FetchGroup(gid string) (*Group, error) {
+	rs := cl.GroupCol.FindOne(
+		cl.Ctx,
+		bson.D{{"_id", gid}},
+	)
+	var grp *Group
+	if rs.Decode(&grp) != nil {
+		return grp, status.New(codes.NotFound, "use not found").Err()
+	}
+	return grp, nil
+}
+
+func (cl *DBClient) FetchGroupAttributes(gid string, attributes bson.D) (*Group, error) {
+	rs := cl.GroupCol.FindOne(
+		cl.Ctx,
+		bson.D{{"_id", gid}},
+		options.FindOne().SetProjection(attributes),
+	)
+	var grp *Group
+	if rs.Decode(&grp) != nil {
+		return grp, status.New(codes.NotFound, "use not found").Err()
+	}
+	return grp, nil
 }

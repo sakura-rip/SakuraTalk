24511d593666ce86f8fc03f0e8a55da85c2d6765
sakura-rip <okaoka1567@gmail.com>
2021-01-28 23:26:51 +0900

    add: python-gen

--- gen-rpc.bat
+++ gen-rpc.bat
@@ -2,3 +2,5 @@ cd /d %~dp0
 protoc --go_out=plugins=grpc:./server/TalkService sakuraTalk.proto
 git add ./server/TalkService
 git commit -m "rpc: generated commit"
+
+python -m grpc.tools.protoc -I. --python_out=./client/TalkService --grpc_python_out=./client/TalkService sakuraTalk.proto

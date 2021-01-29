da5b5fda0b3406c862b521bfbd6e977ecc8e0094
sakura-rip <okaoka1567@gmail.com>
2021-01-30 01:50:01 +0900

    add: GenerateUUID func

--- server/utils/random.go
+++ server/utils/random.go
@@ -1,1 +1,12 @@
 package utils
+
+import (
+	"crypto/md5"
+	"fmt"
+	"github.com/google/uuid"
+)
+
+func GenerateUUID() string {
+	uid, _ := uuid.NewRandom()
+	return fmt.Sprintf("%s", md5.Sum([]byte(uid.String())))
+}

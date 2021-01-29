20f227abf56c63a5b7e229fb27d0dcc4dc3f7253
sakura-rip <okaoka1567@gmail.com>
2021-01-29 20:34:45 +0900

    add: middle ware

--- server/talkServer/run.go
+++ server/talkServer/run.go
@@ -1,12 +1,44 @@
 package talkServer
 
 import (
+	"context"
 	"github.com/sakura-rip/SakuraTalk/TalkService"
+	"github.com/sakura-rip/SakuraTalk/utils"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"net"
 )
 
 type TalkHandler struct{}
+
+type DefaultMiddleWare func(ctx context.Context) (context.Context, error)
+
+func newUnaryServerInterceptor(authFunc DefaultMiddleWare) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		newCtx, err := authFunc(ctx)
+		if err != nil {
+			return nil, status.New(codes.Unauthenticated, "authentication failed").Err()
+		}
+		return handler(newCtx, req)
+	}
+}
+
+func newDefaultMiddleWare() DefaultMiddleWare {
+	return func(ctx context.Context) (context.Context, error) {
+		token, ok := getHeader(ctx, "X-Sakura-Access")
+		if !ok {
+			return ctx, status.New(codes.Unauthenticated, "authentication failed").Err()
+		}
+		jwt, err := auth.VerifyIDToken(ctx, token)
+		if err != nil {
+			return ctx, status.New(codes.Unauthenticated, "authentication failed").Err()
+		}
+		ctx = utils.SetKeyAndValue(ctx, "uuid", jwt.UID)
+		ctx = utils.SetKeyAndValue(ctx, "claims", jwt.Claims)
+		return ctx, nil
+	}
+}
 
 // RunServer サーバーを走らせます！
 func RunServer() {
@@ -14,7 +46,9 @@ func RunServer() {
 	if err != nil {
 		panic(err)
 	}
-	server := grpc.NewServer()
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(newUnaryServerInterceptor(newDefaultMiddleWare())),
+	)
 	talkService := TalkHandler{}
 	TalkService.RegisterTalkServiceServer(server, talkService)
 	if err := server.Serve(listen); err != nil {

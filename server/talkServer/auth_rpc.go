55edd57bab216b80fe9665fa862726500ca546ad
sakura-rip <okaoka1567@gmail.com>
2021-01-30 03:19:31 +0900

    add: RegisterPrimary

--- server/talkServer/auth_rpc.go
+++ server/talkServer/auth_rpc.go
@@ -2,7 +2,12 @@ package talkServer
 
 import (
 	"context"
+	"fmt"
 	service "github.com/sakura-rip/SakuraTalk/talkService"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 )
 
 func (t TalkHandler) RegisterPrimary(ctx context.Context, request *service.RegisterPrimaryRequest) (*service.RegisterPrimaryResponse, error) {

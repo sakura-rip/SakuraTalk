75611fa59ceea6aca937b928773a8b99c7c6b13a
sakura-rip <okaoka1567@gmail.com>
2021-01-30 17:02:18 +0900

    add: setting func

--- server/talkServer/setting_rpc.go
+++ server/talkServer/setting_rpc.go
@@ -3,12 +3,46 @@ package talkServer
 import (
 	"context"
 	service "github.com/sakura-rip/SakuraTalk/talkService"
+	"github.com/sakura-rip/SakuraTalk/utils"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func (t TalkHandler) GetSetting(ctx context.Context, empty *service.Empty) (*service.Setting, error) {
-	panic("implement me")
+	dbSetting, err := dbClient.FetchUserSetting(utils.GetUUID(ctx))
+	if err != nil {
+		return nil, err
+	}
+	return dbSetting.ConvertToRPCStruct(), err
 }
 
 func (t TalkHandler) UpdateSettingAttributes(ctx context.Context, request *service.UpdateSettingAttributesRequest) (*service.UpdateSettingAttributesResponse, error) {
-	panic("implement me")
+	var updateObject bson.D
+	for _, key := range request.Key {
+		switch key {
+		case service.SettingKey_PRIVATE_USER_ID:
+			updateObject = append(updateObject, bson.E{Key: "setting.pUserID", Value: request.Setting.PrivateUserID})
+		case service.SettingKey_EMAIL:
+			updateObject = append(updateObject, bson.E{Key: "setting.email", Value: request.Setting.Email})
+		case service.SettingKey_ALLOW_SEARCH_BY_PRIVATE_USER_ID:
+			updateObject = append(updateObject, bson.E{Key: "setting.asByPUserID", Value: request.Setting.AllowSearchByPrivateUserID})
+		case service.SettingKey_ALLOW_SEARCH_BY_EMAIL:
+			updateObject = append(updateObject, bson.E{Key: "setting.asByEmail", Value: request.Setting.AllowSearchByEmail})
+		case service.SettingKey_ALLOW_SEARCH_BY_USER_TICKET:
+			updateObject = append(updateObject, bson.E{Key: "setting.asByUserTicket", Value: request.Setting.AllowSearchByUserTicket})
+		}
+	}
+	err := dbClient.UpdateUser(utils.GetUUID(ctx), updateObject)
+	if err != nil {
+		return nil, err
+	}
+	return &service.UpdateSettingAttributesResponse{}, nil
+}
+
+func (t TalkHandler) IssueUserTicket(ctx context.Context, request *service.IssueUserTicketRequest) (*service.IssueUserTicketResponse, error) {
+	ticket := utils.GenerateUUID()
+	err := dbClient.UpdateUser(utils.GetUUID(ctx), bson.D{{"setting.UTicket", ticket}})
+	if err != nil {
+		return nil, err
+	}
+	return &service.IssueUserTicketResponse{Ticket: ticket}, nil
 }

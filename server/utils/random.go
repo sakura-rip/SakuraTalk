95e16a960369d7e5781f26ffa422a7fdc799ff40
sakura-rip <okaoka1567@gmail.com>
2021-01-30 14:32:39 +0900

    change: use default uuid

--- server/utils/random.go
+++ server/utils/random.go
@@ -1,12 +1,10 @@
 package utils
 
 import (
-	"crypto/md5"
-	"fmt"
 	"github.com/google/uuid"
 )
 
 func GenerateUUID() string {
 	uid, _ := uuid.NewRandom()
-	return fmt.Sprintf("%s", md5.Sum([]byte(uid.String())))
+	return uid.String()
 }

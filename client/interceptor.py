f5c5ed2718122633d207a56385b0bc4889c04a6f
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-02-10 23:33:20 +0900

    change: move interceptor class

---
+++ client/interceptor.py
@@ -0,0 +1,37 @@
+from typing import Callable, Any, Tuple, List
+
+import grpc
+from grpc_interceptor import ClientInterceptor, ClientCallDetails
+from grpc_interceptor.client import ClientInterceptorReturnType
+
+
+class MetadataClientInterceptor(ClientInterceptor):
+    @property
+    def metadata(self):
+        return self._metadata
+
+    @metadata.setter
+    def metadata(self, v: List[Tuple[str, str]]):
+        self._metadata = v
+
+    def intercept(
+            self,
+            method: Callable,
+            request_or_iterator: Any,
+            call_details: grpc.ClientCallDetails,
+    ) -> ClientInterceptorReturnType:
+        new_details = ClientCallDetails(
+            call_details.method,
+            call_details.timeout,
+            self._metadata,
+            call_details.credentials,
+            call_details.wait_for_ready,
+            call_details.compression,
+        )
+        return method(request_or_iterator, new_details)
+
+
+def newMetadataClientInterceptor(*metadata) -> MetadataClientInterceptor:
+    interceptor = MetadataClientInterceptor()
+    interceptor.metadata = [*metadata]
+    return interceptor

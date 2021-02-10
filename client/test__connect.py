97979a899c67c3eba9f8c29c6b39b3ddd3df6695
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-02-10 23:26:29 +0900

    change: use interceptor

--- client/test__connect.py
+++ client/test__connect.py
@@ -1,14 +1,36 @@
 import json
+from typing import Any, Callable
 
 import grpc
 import requests
+from grpc_interceptor import ClientInterceptor, ClientCallDetails
+from grpc_interceptor.client import ClientInterceptorReturnType
 
 import sakuraTalk_pb2
 import sakuraTalk_pb2_grpc
+from config import Config
+
+
+class MetadataClientInterceptor(ClientInterceptor):
+    def intercept(
+            self,
+            method: Callable,
+            request_or_iterator: Any,
+            call_details: grpc.ClientCallDetails,
+    ) -> ClientInterceptorReturnType:
+        new_details = ClientCallDetails(
+            call_details.method,
+            call_details.timeout,
+            [("x-sakura-access", firebase_token)],
+            call_details.credentials,
+            call_details.wait_for_ready,
+            call_details.compression,
+        )
+        return method(request_or_iterator, new_details)
 
 
 def signUpWithEmailAndPasswd(email: str, password: str):
-    uri = f"https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyAcBzQrmliUXRAxuut3b7j2a1mEXCRpmAs"
+    uri = f"https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key={Config.FIREBASE_AUTH_KEY}"
     headers = {"Content-type": "application/json"}
     data = {"email": email, "password": password, "returnSecureToken": True}
     result = requests.post(
@@ -22,10 +44,10 @@ def signUpWithEmailAndPasswd(email: str, password: str):
 firebase_token = signUpWithEmailAndPasswd(email="fadsfads@gamil.com", password="fadsfasdfas")
 
 channel = grpc.insecure_channel('localhost:8806')
+channel = grpc.intercept_channel(channel, MetadataClientInterceptor())
 stub = sakuraTalk_pb2_grpc.TalkServiceStub(channel)
-primary, call = stub.registerPrimary.with_call(
-    sakuraTalk_pb2.registerPrimaryRequest(token=firebase_token),
-    metadata=(("x-sakura-access", firebase_token),)
+primary = stub.registerPrimary(
+    sakuraTalk_pb2.registerPrimaryRequest(token=firebase_token)
 )
 tag = sakuraTalk_pb2.Tag(
     tagID="",
@@ -35,10 +57,7 @@ tag = sakuraTalk_pb2.Tag(
     creator="creator",
     createdTime=0
 )
-response, call = stub.createTag.with_call(
-    sakuraTalk_pb2.createTagRequest(tag=tag),
-    metadata=(("x-sakura-access", firebase_token),)
+response = stub.createTag(
+    sakuraTalk_pb2.createTagRequest(tag=tag)
 )
 print(response.tagID)
-for k, v in call.trailing_metadata():
-    print(f"k:{k}, v:{v}")

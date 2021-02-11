c67a3e50556e16806a96602a27054f8ee458d07f
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-02-11 13:45:41 +0900

    add: getgroups

--- server/talkServer/group_rpc.go
+++ server/talkServer/group_rpc.go
@@ -9,6 +9,34 @@ import (
 )
 
 func (t TalkHandler) GetGroups(ctx context.Context, request *service.GetGroupsRequest) (*service.GetGroupsResponse, error) {
+	mid := utils.GetMid(ctx)
+	var groups = map[string]*service.Group{}
+	for _, gid := range request.GroupIds {
+		group, err := dbClient.FetchGroup(gid)
+		if err != nil {
+			continue
+		}
+		groupSetting, _ := dbClient.FetchUserGroupSettings(mid, gid)
+		groups[gid] = &service.Group{
+			GroupID:                   gid,
+			GroupName:                 group.Name,
+			OverWrittenName:           groupSetting.OverWrittenName,
+			Creator:                   group.CreatorMID,
+			CreatedTime:               group.CreatedTime,
+			IconPath:                  group.IconPath,
+			CoverPath:                 group.CoverPath,
+			IsFavorite:                groupSetting.IsFavorite,
+			EnableNotification:        groupSetting.EnableNotification,
+			EnableNotificationMention: groupSetting.EnableNotificationMention,
+			EnableNotificationOnJoin:  groupSetting.EnableNotificationOnJoin,
+			EnableNotificationOnKick:  groupSetting.EnableNotificationOnKick,
+			TagIDs:                    groupSetting.TagIds,
+			InvitationTicket:          group.GroupTicket,
+			MemberIds:                 group.MemberMids,
+			InvitedIds:                group.InvitedMids,
+		}
+	}
+	return &service.GetGroupsResponse{Groups: groups}, nil
 }
 
 func (t TalkHandler) CreateGroup(ctx context.Context, request *service.CreateGroupRequest) (*service.CreateGroupResponse, error) {

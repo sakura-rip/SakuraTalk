427d5fa8f2e276f8cec80b9457767a2bd32f5cb3
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-02-11 11:27:34 +0900

    add: get contacts

--- server/talkServer/contact_rpc.go
+++ server/talkServer/contact_rpc.go
@@ -2,11 +2,43 @@ package talkServer
 
 import (
 	"context"
+	"github.com/sakura-rip/SakuraTalk/talkDatabase"
 	service "github.com/sakura-rip/SakuraTalk/talkService"
+	"github.com/sakura-rip/SakuraTalk/utils"
 )
 
 func (t TalkHandler) GetContacts(ctx context.Context, request *service.GetContactsRequest) (*service.GetContactsResponse, error) {
-	panic("implement me")
+	mid := utils.GetMid(ctx)
+	var contacts = map[string]*service.Contact{}
+	for _, targetMid := range request.Mids {
+		targetProfile, err := dbClient.FetchUserProfile(targetMid)
+		if err != nil {
+			continue
+		}
+		targetContact, err := dbClient.FetchUserContact(mid, targetMid)
+		if err != nil {
+			continue
+		}
+		contacts[targetMid] = &service.Contact{
+			Mid:             targetMid,
+			DisplayName:     targetProfile.Name,
+			OverWrittenName: targetContact.OverWrittenName,
+			Bio:             targetProfile.Bio,
+			IconPath:        targetProfile.IconPath,
+			CoverPath:       targetProfile.CoverPath,
+			TwitterID:       targetProfile.TwitterID,
+			InstagramID:     targetProfile.InstagramID,
+			GithubID:        targetProfile.GithubID,
+			WebSiteURL:      targetProfile.WebSiteURL,
+			Location:        targetProfile.Location,
+			BirthDay:        targetProfile.BirthDay,
+			Status:          targetProfile.Status,
+			ContactStatus:   talkDatabase.RPCContactStatusFromDB(targetContact.ContactStatus),
+			IsFavorite:      targetContact.IsFavorite,
+			TagIds:          targetContact.TagIds,
+		}
+	}
+	return &service.GetContactsResponse{Contacts: contacts}, nil
 }
 
 func (t TalkHandler) UpdateContact(ctx context.Context, request *service.UpdateContactRequest) (*service.UpdateContactResponse, error) {

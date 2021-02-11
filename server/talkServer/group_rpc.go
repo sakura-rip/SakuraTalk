9b42b07f8e26c9ae6f057be492cd29a0d25ccb54
さくらだぞ！ <56917703+sakura-rip@users.noreply.github.com>
2021-02-11 12:29:16 +0900

    add: Group func

--- server/talkServer/group_rpc.go
+++ server/talkServer/group_rpc.go
@@ -8,3 +8,19 @@ import (
 func (t TalkHandler) GetGroup(ctx context.Context, request *service.GetGroupRequest) (*service.GetGroupResponse, error) {
 	panic("implement me")
 }
+
+func (t TalkHandler) CreateGroup(ctx context.Context, request *service.CreateGroupRequest) (*service.CreateGroupResponse, error) {
+	panic("implement me")
+}
+
+func (t TalkHandler) UpdateGroup(ctx context.Context, request *service.UpdateGroupRequest) (*service.UpdateGroupResponse, error) {
+	panic("implement me")
+}
+
+func (t TalkHandler) InviteIntoGroup(ctx context.Context, request *service.InviteIntoGroupRequest) (*service.InviteIntoGroupResponse, error) {
+	panic("implement me")
+}
+
+func (t TalkHandler) KickoutFromGroup(ctx context.Context, request *service.KickoutFromGroupRequest) (*service.KickoutFromGroupResponse, error) {
+	panic("implement me")
+}
